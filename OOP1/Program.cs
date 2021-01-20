7c28d6139991a55f65d04012770d61e805171aea
Aykut Öztürk <aykut.ozturk@hotmail.com>
2021-01-20 23:50:50 +0300

    20 Ocak Ders Sonu

---
+++ OOP1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;//mobilya
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, ProductName = "Kalem", UnitPrice = 35, UnitsInStock = 5 };
+
+            //Pascal case - kelimenin ilk harfi büyük(class)
+            //Camel case - ilk harf küçük diğer kelimenin ilk harfi büyük
+            //case sensitive - küçük büyük harf duyarlı
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);//referans tip - kamera
+            Console.WriteLine(product1.ProductName);
+
+            productManager.Topla2(3, 6);
+
+            //int sayi = 100;
+            //productManager.BiseyYap(sayi);
+            //Console.WriteLine(sayi);
+
+            //int,double,bool ...değer tip
+            //diziler,class,abstract class, interface .. referans tip
+            //ref out
+        }
+    }
+}
